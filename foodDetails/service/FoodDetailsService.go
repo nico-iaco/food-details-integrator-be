6457d85d71aa1f96d1a6fdbc0d11811451bcaaac
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-09-27 22:40:41 +0200

    added cache to avoid multiple calls to external api

--- foodDetails/service/FoodDetailsService.go
+++ foodDetails/service/FoodDetailsService.go
@@ -5,39 +5,53 @@ import (
 	"food-details-integrator-be/foodDetails/model"
 	"github.com/mashingan/smapping"
 	"github.com/openfoodfacts/openfoodfacts-go"
+	"os"
 )
 
 type FoodDetailsService struct {
+	CacheService CacheService
 }
 
 var fds *FoodDetailsService = nil
 
-func New() FoodDetailsService {
+func NewFoodDetailsService(cs CacheService) FoodDetailsService {
 	if fds == nil {
 		fds = new(FoodDetailsService)
+		fds.CacheService = cs
 	}
 	return *fds
 }
 
 func (s FoodDetailsService) GetProductDetails(barcode string) (model.FoodDetails, error) {
 	api := openfoodfacts.NewClient("world", "", "")
-	//api.Sandbox()
-	product, err := api.Product(barcode)
 	foodDetails := model.FoodDetails{}
-	if err != nil {
-		fmt.Printf("error %s \n", err)
-		return model.FoodDetails{}, err
-	} else {
-		mappedField := smapping.MapTags(product, "json")
-		err := smapping.FillStructByTags(&foodDetails, mappedField, "json")
-		if err != nil {
-			return model.FoodDetails{}, err
-		}
-		foodDetails.ImageURL = product.ImageURL.URL.String()
-		foodDetails.ImageNutritionURL = product.ImageNutritionURL.URL.String()
 
+	if os.Getenv("IS_SANDBOX") == "true" {
+		api.Sandbox()
 	}
-	return foodDetails, nil
+
+	err := cs.GetObject(barcode, &foodDetails)
+
+	if err != nil {
+		product, err := api.Product(barcode)
+
+		if err != nil {
+			fmt.Printf("error %s \n", err)
+			return model.FoodDetails{}, err
+		} else {
+			mappedField := smapping.MapTags(product, "json")
+			err := smapping.FillStructByTags(&foodDetails, mappedField, "json")
+			if err != nil {
+				return model.FoodDetails{}, err
+			}
+			foodDetails.ImageURL = product.ImageURL.URL.String()
+			foodDetails.ImageNutritionURL = product.ImageNutritionURL.URL.String()
+		}
+	} else {
+		fmt.Println("cache hit")
+	}
+	err = cs.SetObject(barcode, foodDetails)
+	return foodDetails, err
 }
 
 func (s FoodDetailsService) GetKcalForFoodQuantity(barcode string, quantity float64, unit string) (float64, error) {
